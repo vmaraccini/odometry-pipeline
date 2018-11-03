2e11405b557a2f4ec63f1a285d647160b10606be
Victor <vgm.maraccini@gmail.com>
2018-11-03 12:17:08 -0300

    Fine-tune features detector
    Add secondary detector based on goodFeaturesToTrack

--- extraction/extractor.py
+++ extraction/extractor.py
@@ -3,8 +3,17 @@ import numpy as np
 
 
 class FeatureExtractor:
-    feature = cv2.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)
+    feature = cv2.FastFeatureDetector_create(threshold=5, nonmaxSuppression=True)
 
     def extract(self, frame):
         features = self.feature.detect(frame)
         return np.array([x.pt for x in features], dtype=np.float32)
+
+class GoodFeaturesExtractor:
+    def extract(self, frame):
+        features = cv2.goodFeaturesToTrack(frame.astype(np.uint8),
+                                           3000,
+                                           qualityLevel=0.05,
+                                           minDistance=8)
+
+        return np.array([f[0] for f in features])
